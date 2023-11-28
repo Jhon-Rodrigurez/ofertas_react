6e47f128c8cdf00780e164af0b090dcb8f00e18b
Jhon-Rodrigurez <116046489+Jhon-Rodrigurez@users.noreply.github.com>
2023-11-28 15:52:39 -0500

    Delete src directory

--- src/states/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from './sliceReducers';
-
-const store= configureStore({
-    reducer: rootReducer
-});
-console.log(store.getState().usuario);
-
-export {store}
